d5ca8249308ac942733c9ebc025761ceefc919e9
王家葳 <forme.wjw@aliyun.com>
2018-11-05 16:30:48 +0800

    update

--- Node/Spider/template.py
+++ Node/Spider/template.py
@@ -15,24 +15,84 @@
 
 from redis import StrictRedis
 
-# 导入 xxxSpider.py里的 xxx 模块
-
-from {0} import {1}
+# 加入导入的 demoSeed.py 的 demoSeed
 
 
-def template():
-    """ 链接redis，或者redis的操作
-    """
-    pass
+
+# 先写好消息中心的代码
+
+class MsgCenter():
+
+    def connect_redis(self):
+        try:
+            redis_cli = StrictRedis(host='loacalhost', port=6379, db=1)
+        except:
+            print('链接\tRedis\t失败....')
+            redis_cli = None
+        return redis_cli
+
+    def push_msg_2_que(self, que, msg):
+        """将数据推入到指定的队列里
+
+        :param que: 指定的队列
+        :param msg: json格式的数据
+        :return 是否成功推入数据
+        """
+
+        done = False
+        redis_cli = self.connect_redis()
+        if redis_cli is not None:
+            redis_cli.lpush(que, msg)
+            done = True
+        return done
+
+    def receive_msg_from_que(self, que):
+        """从指定的队列里获取消息
+
+        没有消息，就等候，
+        :param que: 指定的队列
+        :return: msg: 消息队列里的数据，没有则为None
+        """
+
+        msg = None
+        redis_cli = self.connect_redis()
+        if redis_cli is not None:
+            msg = redis_cli.rpop(que)
+            # 没有数据则为None
+        return msg
 
 
-def do():
-    """主方法
-    """
-    # 实例化该模块，然后调用，同时监听和输出对应的队列
-    instance = {2}()
-    instance.do()
+# 执行部分，这次写一个seed 的demo
 
+class DemoSeed():
+
+    def execute(self):
+        seed = 'test'
+        return seed
+
+seed_in = 'demo_seedin'
+
+seed_out = 'demo_seedout'
+
+import time
+
+def execute():
+    msgc = MsgCenter()
+    sed = DemoSeed()
+
+
+    while True:
+
+        # 获取消息
+        msg = msgc.receive_msg_from_que(seed_in)
+        if msg is not None:
+            # 获取种子
+            seed = sed.execute()
+            # 放入队列
+            msgc.push_msg_2_que(seed_out, seed)
+        else:
+            time.sleep(0.1)
+            continue
 
 if __name__ == '__main__':
-    do()
+    execute()
--- Node/micro_script_maker.py
+++ Node/micro_script_maker.py
@@ -9,11 +9,63 @@
     该脚本的功能就是，在 ./spider/xxxx/xxxxspider.py下
     写对于模块的小程序
     目的是方便 nodeExecutor 来启动
+    TaskReader在读任务的时候，就已经生成队列列表
+
+    demoSpider: {
+        'demoseed': {
+            'in': 'demo_seedin',
+            'out': 'demo_seedout',
+        },
+        'demodownloader': {
+            'in': 'demo_downin',
+            'out': 'demo_downout',
+        },
+        'demoparse': {
+            'in': 'demo_parsein',
+            'out_url': 'demo_parseurlout',
+            'out_data': 'demo_parserdataout',
+        }
+        'demo_persistence': {
+            'in': 'demo_persisin'
+        }
+    }
+    可以看出，每个脚本8个队列
+
     ########################################################
     2018-11-02 配置一个模板来实现
+    ########################################################
+    2018-11-05 完成这个部分的编写
+    要做的事情:
+        1. 接收到任务
+        2. 开始生成 xxx_seed.py、xxx_downloader.py、xxx_parser.py、xxx_persistence.py
+        3. 生成过程中，就已经为各个小脚本配置好同消息中心通信的队列
+            parser是个另外，因为涉及到，反馈url的问题
+        4. 暂时不写通用的模板，一个部分一个部分的写
 """
 
-template_path = './spider/template.txt'
-template = open(template_path, 'r', encoding='utf8').read()
 
-print(template)
+class MsgCenter():
+    """作为消息中心的
+    车间，装配
+    """
+
+    def __init__(self, project):
+        # 项目名称
+        self.project_name = project
+
+    def seed_module(self):
+        """
+        为seed模块匹配一个队列
+        """
+        que_in = '{0}_seedin'.format(self.project_name)
+        que_out = '{0}_seedout'.format(self.project_name)
+
+        script = """
+        
+        # 这是种子模块的通信代码
+        # 接受到消息，执行seed代码，获取新种子
+        # 将新种子放入队列里
+        
+        
+        
+        """
