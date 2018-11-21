89b27a357190c5c9de8915a5723cbd96bbbe9c8f
wangjiawei <forme.wjw@aliyun.com>
2018-11-21 11:46:24 +0800

    开始撸task_executor

---
+++ Node/TaskExecutor/__init__.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+__author__ = 'wangjiawei'
+__date__ = '2018-11-22'
+
+"""
+    TaskExecutor作为任务执行者
+
+    1. 从task_index.ini 读取任务
+    随后调用ps相应接口，启动任务
+
+    2. 同样从task_index.ini 读取各个任务启动状态
+    生成kill_list 随后调用相应ps接口，kill掉进程
+"""
